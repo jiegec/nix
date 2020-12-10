4bdd25879722227f7c9ac6e774c6d5e01b069cd7
Jiajie Chen <c@jia.je>
2020-12-10 14:21:36 +0800

    Cleanup unneeded nix files

--- overlay.nix
+++ overlay.nix
@@ -1,25 +1,5 @@
 self: super:
 
 {
-  patchmacho = self.callPackage ./patchmacho.nix {};
   riscv64-unknown-elf = self.callPackage ./riscv64-unknown-elf.nix {};
-  iproute2mac = self.callPackage ./iproute2mac.nix {};
-  powerline-status = self.callPackage ./powerline-status.nix {
-    buildPythonPackage = self.python38Packages.buildPythonPackage;
-    fetchPypi = self.python38Packages.fetchPypi;
-  };
-
-  default-env = super.buildEnv {
-    name = "default-env";
-    paths = [
-      (self.python38.withPackages (
-        ps: [
-          self.powerline-status
-          ps.pylint
-          ps.autopep8
-          ps.pyserial
-        ]
-      ))
-    ];
-  };
 }
