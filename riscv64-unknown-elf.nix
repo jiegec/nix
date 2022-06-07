72e666f6cd6d78d58ec86b1643fa7cfa7f33f8c4
Jiajie Chen <c@jia.je>
2022-06-07 09:01:09 +0800

    Bump riscv64-unknown-elf version

--- riscv64-unknown-elf.nix
+++ riscv64-unknown-elf.nix
@@ -2,19 +2,26 @@
 
 stdenv.mkDerivation {
   pname = "riscv64-unknown-elf";
-  version = "8.3.0-2020.04.0";
+  version = "10.2.0-2020.12.8";
 
   src = fetchurl {
-    url = "https://static.dev.sifive.com/dev-tools/riscv64-unknown-elf-gcc-8.3.0-2020.04.0-x86_64-apple-darwin.tar.gz";
-    sha256 = "f7b5d1ff92b1fe0db90295ba02e8f684c5d8fc3c26d01cef6b376668053fc038";
+    url = "https://static.dev.sifive.com/dev-tools/freedom-tools/v2020.12/riscv64-unknown-elf-toolchain-10.2.0-2020.12.8-x86_64-apple-darwin.tar.gz";
+    sha256 = "d0fc75a76cb2873576bc49f0014011de4dac24cd63ce4c67c985aa37167cf730";
   };
 
   nativeBuildInputs = [ ];
   buildInputs = [ xz.dev ];
 
   installPhase = ''
-    mkdir "$out"
-    cp -rp . "$out"
-    install_name_tool -change /usr/local/opt/xz/lib/liblzma.5.dylib ${xz.out}/lib/liblzma.5.dylib $out/bin/riscv64-unknown-elf-gdb 
+    mkdir -p "$out/toolchain"
+    cp -rp . "$out/toolchain"
+
+    mkdir -p "$out/bin"
+    cd "$out/toolchain/bin"
+    for binary in *; do
+        ln -s "$out/toolchain/bin/$binary" "$out/bin/$binary"
+    done
+
+    install_name_tool -change /usr/local/opt/xz/lib/liblzma.5.dylib ${xz.out}/lib/liblzma.5.dylib $out/toolchain/bin/riscv64-unknown-elf-gdb 
   '';
 }
