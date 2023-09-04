603db4069ddb1b48d3972cf96a477bfd245fb5dd
Jiajie Chen <c@jia.je>
2023-09-04 21:37:21 +0800

    Add ct-ng to hydra jobs

--- flake.nix
+++ flake.nix
@@ -1,11 +1,13 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: rec {
     packages.x86_64-linux.cajviewer = nixpkgs.legacyPackages.x86_64-linux.callPackage ./cajviewer.nix { };
 
     packages.x86_64-linux.crosstool-ng = nixpkgs.legacyPackages.x86_64-linux.callPackage ./crosstool-ng.nix { };
 
     packages.x86_64-darwin.riscv64-unknown-elf = nixpkgs.legacyPackages.x86_64-darwin.callPackage ./riscv64-unknown-elf.nix { };
+
+    hydraJobs.crosstool-ng.x86_64-linux = packages.x86_64-linux.crosstool-ng;
   };
 }
